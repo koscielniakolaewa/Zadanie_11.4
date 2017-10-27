ba67ca4a4c7de5464f1a4495290f958b6089e837
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-27 12:01:18 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,35 @@ function Phone(brand, price, color, dodatek) {
 	this.dodatek = dodatek;
 }
 
-var Samsung Galaxy S6 = new Phone("Samsung", 2250, "niebieski", "obudowa");
-var iPhone6S = new Phone("Apple", 3000, "różowy", "sluchawki");
-var OnePlus One = new Phone("OnePlus", 1500, "czarny", "ladowarka");
-
-Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "it also has" + this.dodatek + ".");
+function Phone1(brand, price, color, dodatek) {
+	this.brand = brand
+	this.price = price;
+	this.color = color;
+	this.dodatek = dodatek;
 }
 
+function Phone2(brand, price, color, dodatek) {
+	this.brand = brand
+	this.price = price;
+	this.color = color;
+	this.dodatek = dodatek;
+}
+
+var iPhone6S = new Phone("Apple", 3000, "różowy", "sluchawki");
+var SamsungGalaxyS6 = new Phone1("Samsung", 2250, "niebieski", "obudowa");
+var OnePlusOne = new Phone2("OnePlus", 1500, "czarny", "ladowarka");
+
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " it also has " + this.dodatek + ".");
+}
 iPhone6S.printInfo();
+
+Phone1.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " it also has " + this.dodatek + ".");
+}
+SamsungGalaxyS6.printInfo();
+
+Phone2.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " it also has " + this.dodatek + ".");
+}
+OnePlusOne.printInfo();
