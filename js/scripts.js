cc5b366ea18c1ca54651e7d05e83e8c1df2b8795
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-27 11:56:48 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Phone(brand, price, color) {
+	this.brand = brand
+	this.price = price;
+	this.color = color;
+	this.dodatek = dodatek;
+}
+
+var Samsung Galaxy S6 = new Phone("Samsung", 2250, "niebieski", "obudowa");
+var iPhone6S = new Phone("Apple", 3000, "różowy", "sluchawki");
+var OnePlus One = new Phone("OnePlus", 1500, "czarny", "ladowarka");
+
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "it also has" + this.dodatek + ".");
+}
+
+iPhone6S.printInfo();
